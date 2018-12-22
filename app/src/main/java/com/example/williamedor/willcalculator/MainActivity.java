45a8af2d53745b7bc53f4822f6b950e4aa99a923
William Edor <wsedor@wpi.edu>
2018-12-22 01:41:59 -0500

    calculator image and code clean up

--- app/src/main/java/com/example/williamedor/willcalculator/MainActivity.java
+++ app/src/main/java/com/example/williamedor/willcalculator/MainActivity.java
@@ -5,10 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
